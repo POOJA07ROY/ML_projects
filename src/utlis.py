41fd1540667c44b86f1b5564f709e709c44808d6
POOJA07ROY <13a1015rpoojaroy@gmail.com>
2024-03-08 15:31:08 +0530

    Hyperparameter Training

--- src/utlis.py
+++ src/utlis.py
@@ -6,7 +6,7 @@ from exception import CustomException
 import dill
 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -19,14 +19,21 @@ def save_object(file_path,obj):
     except Exception as e :
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,Y_train,X_test,Y_test,models):
+def evaluate_models(X_train,Y_train,X_test,Y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
-            model.fit(X_train,Y_train) #train moodel
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,Y_train)
+
+            # model.fit(X_train,Y_train) #train moodel
 
             Y_train_pred = model.predict(X_train)
 
